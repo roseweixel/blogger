8c2655bf990110b461292d217352af9de77300c4
Rose Weixel <rose.weixel@gmail.com>
2015-05-11 09:38:39 -0400

    clean up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,11 +50,4 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
-
-  # config.action_mailer.default_url_options = {
-  #     :host => "localhost:3000"
-  # }
-
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
 end
