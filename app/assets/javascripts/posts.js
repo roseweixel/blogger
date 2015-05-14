20c765fd9c9ef14682d7c38b79b7e07864e88097
Rose Weixel <rose.weixel@gmail.com>
2015-05-14 15:04:36 -0400

    search for post by title or author

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -6,8 +6,9 @@ $(function(){
   });
 
   $('#query').on('keyup', function(){
-    var query = this.value
-    $('.post-title:not(:contains('+query+'))').parent().hide();
-    $('.post-title:contains('+query+')').parent().show();
-  })
+    var query = this.value;
+    var searchAttribute = $('#search_options').val().match(/\w+$/)[0];
+    $('.post-' + searchAttribute + ':not(:contains(' + query + '))').parent().hide();
+    $('.post-' + searchAttribute + ':contains(' + query + ')').parent().show();
+  });
 });
